0dd98c404800ac04f5bf6f63fd566617b44090d3
Uakyt Arai <arai.uakit@mail.com>
2026-09-13 22:12:32 +0500

    add demo Main exercising director and builders

--- indie-game-quest-builder/src/com/questforge/Main.java
+++ indie-game-quest-builder/src/com/questforge/Main.java
@@ -1,17 +1,54 @@
 package com.questforge;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.questforge.builder.DailyQuestBuilder;
+import com.questforge.builder.MainQuestBuilder;
+import com.questforge.builder.SideQuestBuilder;
+import com.questforge.director.QuestDirector;
+import com.questforge.exception.InvalidQuestException;
+import com.questforge.model.Quest;
+import com.questforge.model.Reward;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("### Using Director with a MainQuestBuilder ###");
+        QuestDirector mainDirector = new QuestDirector(new MainQuestBuilder());
+        Quest tutorial = mainDirector.constructTutorialQuest();
+        System.out.println(tutorial);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("### Using Director with a SideQuestBuilder ###");
+        QuestDirector sideDirector = new QuestDirector(new SideQuestBuilder());
+        Quest herbs = sideDirector.constructHerbGatheringQuest();
+        System.out.println(herbs);
+
+        System.out.println("### Using Director with a DailyQuestBuilder ###");
+        QuestDirector dailyDirector = new QuestDirector(new DailyQuestBuilder());
+        Quest wolves = dailyDirector.constructWolfHuntDailyQuest();
+        System.out.println(wolves);
+
+        System.out.println("### Manual building without Director ###");
+        Quest customSide = new SideQuestBuilder()
+                .setTitle("Lost Kitten")
+                .setDescription("Find the blacksmith's lost kitten.")
+                .setLocation("Eldoria Village")
+                .setRequiredLevel(2)
+                .setTimeLimitMinutes(15)
+                .addObjective("Search the barn")
+                .addObjective("Search the well")
+                .addReward(new Reward("Gold", 10))
+                .build();
+        System.out.println(customSide);
+
+        System.out.println("### Demonstrating validation failure ###");
+        try {
+            new MainQuestBuilder()
+                    .setTitle("Broken Main Quest")
+                    .setDescription("Missing objectives on purpose.")
+                    .setLocation("Nowhere")
+                    .setRequiredLevel(1)
+                    .addReward(new Reward("Gold", 1))
+                    .build();
+        } catch (InvalidQuestException ex) {
+            System.out.println("Expected failure: " + ex.getMessage());
         }
     }
 }
